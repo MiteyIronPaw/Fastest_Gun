d1580ffc497bdc19149a66a03c278c34b9a40493
MiteyIronPaw <grayghost.ingress@gmail.com>
2015-01-20 23:42:09 +1300

    First commit

---
+++ timer.js
@@ -0,0 +1,15 @@
+time = 0;
+stopped_time = 0;
+
+function start_time(){
+    time = Date.now();
+}
+
+function get_time(){
+    if(time == 0){return 0;}
+    return (Date.now()-time)/1000;
+}
+
+function stop_time(){
+    stopped_time = get_time();
+}
